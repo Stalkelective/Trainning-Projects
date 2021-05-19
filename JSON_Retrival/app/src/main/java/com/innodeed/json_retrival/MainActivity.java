530fb7f1a86f068400009e99010d2a75e2dc3c50
AMAN SARJAL <83810448+Stalkelective@users.noreply.github.com>
2021-05-19 15:46:52 +0530

    Add files via upload

---
+++ JSON_Retrival/app/src/main/java/com/innodeed/json_retrival/MainActivity.java
@@ -0,0 +1,58 @@
+package com.innodeed.json_retrival;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity  {
+    TextView textView;
+   // String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.txt);
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos/1", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                   String data =response.getString("title");
+                    textView.setText(data);
+                    Log.d("myapp","The response is " + response.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("myapp","Something went Wrong");
+            }
+        });
+
+       requestQueue.add(jsonObjectRequest);
+
+    }
+
+
+}
