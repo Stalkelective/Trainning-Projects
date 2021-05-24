148c02edc95cca8ab3a6238f6a95094dda9ae54b
AMAN SARJAL <83810448+Stalkelective@users.noreply.github.com>
2021-05-24 21:11:34 +0530

    Add files via upload

---
+++ Drawing_Images_Using_Canvas/app/src/main/java/com/innodeed/drawing_images_using_canvas/CustomView.java
@@ -0,0 +1,54 @@
+package com.innodeed.drawing_images_using_canvas;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+    private Bitmap bitmap;
+
+    public CustomView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet set) {
+        bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.mango);
+
+        if (set == null) {
+            return;// to avoid null pointer exception
+        }
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(bitmap, 55, 300, null);
+    }
+}
---
+++ Drawing_Images_Using_Canvas/app/src/main/java/com/innodeed/drawing_images_using_canvas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.innodeed.drawing_images_using_canvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
